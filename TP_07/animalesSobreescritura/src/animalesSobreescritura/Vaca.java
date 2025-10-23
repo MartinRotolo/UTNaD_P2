fad4615f635f0198ef794e0b1e3888683227f349
Martin Rotolo <martinrotolo1997@gmail.com>
2025-10-23 21:32:38 +0200

    Resuelto

---
+++ TP_07/animalesSobreescritura/src/animalesSobreescritura/Vaca.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animalesSobreescritura;
+
+/**
+ *
+ * @author Martin
+ */
+public class Vaca extends Animal {
+    @Override
+    public void hacerSonido() {
+        System.out.println("¡Muuu!");
+    }
+}
